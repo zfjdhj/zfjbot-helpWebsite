1b064c2f5878d7de40100bd1630c1cc8c71c6315
Administrator <320336328@qq.com>
2020-12-09 15:59:43 +0800

    a simple vue demo

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import less from 'less'
+import  {Search,Button, Collapse, CollapseItem,Cell,CellGroup,NavBar,NoticeBar,Sticky,Divider } from "vant"
+import 'vant/lib/index.css';
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(Collapse).use(CollapseItem).use(Button).use(Search).use(Cell)
+    .use(CellGroup).use(NavBar).use(NoticeBar).use(Sticky).use(Divider)
+    .use(less)
+    .mount('#app')
